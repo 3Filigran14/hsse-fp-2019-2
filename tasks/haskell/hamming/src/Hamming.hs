7851fc7d3e091750e9601f148d0ca3531ab9c1db
Nikita Filigran <koteyko94@gmail.com>
2019-12-26 16:25:02 +0300

    Update Hamming.hs

--- tasks/haskell/hamming/src/Hamming.hs
+++ tasks/haskell/hamming/src/Hamming.hs
@@ -1,8 +1,5 @@
 module Hamming (distance) where
-
 distance :: String -> String -> Maybe Int
-distance (x : xs) (y : ys)
-    | x /= y    = (+) 1 <$> distance xs ys
-    | otherwise = distance xs ys
-distance [] []  = Just 0
-distance _ _    = Nothing
+distance xs ys = if ((length xs) /= (length ys))
+then Nothing
+else Just (length (filter (uncurry (/=)) (zip xs ys)))
